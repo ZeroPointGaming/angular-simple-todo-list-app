afd21b463b427f8d52b75b12081c350914b047f7
Justin Hedrick <jhedrick@elykinnovation.com>
2023-06-23 11:05:40 -0400

    Added working project code - init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list-app';
+  list = Array();
+
+  addItem(v: string): void {
+    //Prevent adding duplicates
+    if (this.list.includes(v)) 
+      return;
+
+    //Push the item to the array
+    this.list.push(v);
+  }
+
+  removeItem(v: string): void {
+    this.list.splice(this.list.indexOf(v), 1)
+  }
+
+  editItem(v: string): void {
+    let e_input = document.getElementById(v);
+    e_input?.removeAttribute('hidden');
+  }
+
+  saveItem(v: string, o: string): void {
+    this.list[this.list.indexOf(o)] = v;
+  }
 }
